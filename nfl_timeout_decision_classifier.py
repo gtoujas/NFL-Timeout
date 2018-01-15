fac0a71af09955d45c6d0e8aa4974afba73a280e
gtoujas <34067888+gtoujas@users.noreply.github.com>
2018-01-15 11:57:35 -0500

    Add files via upload
    drop rows with null values from potential training set

--- nfl_timeout_decision_classifier.py
+++ nfl_timeout_decision_classifier.py
@@ -6,13 +6,22 @@ import pandas as pd
 initial_df = pd.read_csv('NFL Play by Play 2009-2016 (v3).csv')
 
 #use functions in cleaning file to clean the raw data from kaggle
-cleaned_df = add_custom_features(initial_df)
-cleaned_df = add_shifted_columns(cleaned_df)
-cleaned_df = add_timeout_label(cleaned_df)
-cleaned_df = remove_games_with_negative_timeouts(cleaned_df)
 
+def cleandata(dataframe):
 
-cleaned_df.to_csv('clean_nfl_data.csv',sep=',',index=False)
+    cleaned_df = add_custom_features(dataframe)
+    cleaned_df = fill_na_with_previous_value(cleaned_df)
+    cleaned_df = add_shifted_columns(cleaned_df)
+    cleaned_df = add_timeout_label(cleaned_df)
+    cleaned_df = remove_games_with_negative_timeouts(cleaned_df)
+    cleaned_df.to_csv('clean_nfl_data.csv',sep=',',index=False)
+
+    return cleaned_df
+
+#cleaned_df = cleandata(initial_df)
+
+cleaned_df = pd.read_csv('clean_nfl_data.csv',sep=',')
+
 
 
 #limit plays to only the second half of both the second quarter and fourth quarter, where teams are most likely to be using timeouts to stop the clock
@@ -25,13 +34,31 @@ time_shortened_df = cleaned_df.query('(450 > TimeSecs > 0) | (2250 > TimeSecs > 
 #keep only the columns we think could possibly be relevant for now
 
 relevant_columns = ['Date','GameID','HomeTeam','AwayTeam','posteam','DefensiveTeam',
-                    'PosTeamScore','DefTeamScore','ScoreDiff','AbsScoreDiff',
+                    'PosTeamScore','DefTeamScore','ScoreDiff','AbsScoreDiff','Possession_Difference',
                     'qtr','time','TimeSecs','PlayTimeDiff','down','ydstogo','yrdline100',
+                    'down_s','ydstogo_s',
                     'desc','PlayType','PassOutcome','RushAttempt',
                     'Accepted.Penalty','PenalizedTeam',
-                    'Timeout Label','Timeout_Indicator_s','Timeout_Team_s','posteam_timeouts_pre_s',
+                    'Timeout_Label','Pos_Timeout_Label','Def_Timeout_Label',
+                    'Timeout_Indicator_s','Timeout_Team_s','posteam_timeouts_pre_s','defteam_timeouts_pre_s',
                     'HomeTimeouts_Remaining_Pre_s','AwayTimeouts_Remaining_Pre_s','HomeTimeouts_Remaining_Post_s','AwayTimeouts_Remaining_Post_s'
                     ]
 
 first_relevant_df = pd.DataFrame(time_shortened_df,columns=relevant_columns)
+
+#potentially think about splitting first and second half?
+#have to split offensive and defensive timeouts, both are very different
+
+#start with very simple features to train basic tree and view results before doing some feature selection
+
+Def_X_df = pd.DataFrame(first_relevant_df,columns=['TimeSecs','Possession_Difference','down_s','ydstogo_s','yrdline100','defteam_timeouts_pre_s'])
+
+
+#need to drop rows with missing values
+
+nulls_df = Def_X_df[pd.isnull(Def_X_df).any(axis=1)]
+na_rows = nulls_df.__len__()
+print('Rows with null values to be deleted: ' + str(na_rows))
+
+Def_X_df=Def_X_df.dropna(axis=0)
 
