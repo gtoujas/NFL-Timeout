6e9b54f8da5649214335f1685781c6e3b20f418b
gtoujas <34067888+gtoujas@users.noreply.github.com>
2018-01-23 23:05:56 -0500

    Add files via upload
    added cross validation and visual representation of the AUC ROC metric

--- nfl_timeout_decision_classifier.py
+++ nfl_timeout_decision_classifier.py
@@ -1,6 +1,6 @@
 from nfl_data_cleaning import *
 import pandas as pd
-
+import numpy as np
 
 """
 
@@ -199,7 +199,7 @@ clf_grid = GridSearchCV(clf_grid,parameters,scoring='roc_auc')
 clf_grid.fit(X_train,y_train)
 grid_params = clf_grid.best_params_
 
-#clf_grid = clf_grid.best_estimator_
+clf_grid = clf_grid.best_estimator_
 
 
 print "Best GridSearch Parameters -------"
@@ -227,6 +227,69 @@ graph_dot = tree.export_graphviz(clf,
 graph = graphviz.Source(graph_dot)
 
 """
-Visualize the ROC curve of the best parameter.
+Visualize the ROC curve of the best parameter with plotting
+
+Sample code taken from http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html#sphx-glr-auto-examples-model-selection-plot-roc-crossval-py
 """
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+import matplotlib.pyplot as plt
+
+
+cv = StratifiedKFold(n_splits=15,random_state=15)
+classifier = tree.DecisionTreeClassifier(criterion = 'entropy',
+                                  splitter = 'best',
+                                  min_impurity_split = .05,
+                                  min_samples_leaf=8,
+                                  min_samples_split = 5,
+                                  max_features = 7,
+                                  max_depth = 9,
+                                  class_weight = {0:.35,1:.65},
+                                  )
+
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0,1,100)
+
+i = 0
+
+for train, test in cv.split(Def_X_df,Def_y_df):
+
+    clf = classifier.fit(Def_X_df.iloc[train],Def_y_df.iloc[train])
+    probas_ = clf.predict_proba(Def_X_df.iloc[test])
+    # Compute ROC curve and area under the curve
+    fpr, tpr, thresholds = roc_curve(Def_y_df.iloc[test],probas_[:,1])
+    tprs.append(interp(mean_fpr,fpr,tpr))
+    tprs[-1][0] = 0.0
+    roc_auc = auc(fpr,tpr)
+    aucs.append(roc_auc)
+    plt.plot(fpr, tpr, lw=1, alpha=0.3,
+             #label = 'ROC fold %d (AUC = %0.2f)' % (i,roc_auc)
+             )
+
+    i += 1
+
+plt.plot([0,1],[0,1], linestyle = '--', lw = 2, color = 'r', label = 'Luck', alpha = .8)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr,mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr,mean_tpr, color= 'b', label = r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc,std_auc),
+         lw=2, alpha=.8)
+
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr,1)
+tprs_lower = np.maximum(mean_tpr - std_tpr,0)
+plt.fill_between(mean_fpr,tprs_lower,tprs_upper, color = 'grey', alpha = .2, label = r'$\pm$ 1 std. dev.')
+
+plt.xlim([-0.05,1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Analysis of Decision Classifier')
+plt.legend(loc = "lower right")
+plt.show()
 
